93b2aaaffcc58fb83576ac9af7a06d05e0ac8d60
RIchard Oluwadamilola <oluwatobyrichard@gmail.com>
2022-10-05 11:57:36 +0100

    Update stack_func3.c

--- stack_func3.c
+++ stack_func3.c
@@ -77,3 +77,33 @@ void _rotr(stack_t **stack, unsigned int line_number)
 
 	runner->n = aux1;
 }
+
+/**
+ * _queue - sets the format of the data to a queue (FIFO)
+ *
+ * @doubly: head of the linked list
+ * @cline: line number;
+ * Return: no return
+ */
+void _queue(stack_t **doubly, unsigned int cline)
+{
+	(void)doubly;
+	(void)cline;
+
+	vglo.lifo = 0;
+}
+
+/**
+ * _stack - sets the format fo the data to a stack (LIFO)
+ *
+ * @doubly: head of the linked list
+ * @cline: line number;
+ * Return: no return
+ */
+void _stack(stack_t **doubly, unsigned int cline)
+{
+	(void)doubly;
+	(void)cline;
+
+	vglo.lifo = 1;
+}
